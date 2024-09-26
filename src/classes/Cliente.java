12d7dad8a5a5fdeea20cc586c106b8d8edeb19d2
Carlos De la Rosa <candela2920@gmail.com>
2024-09-25 23:55:46 -0500

    List Logger added

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -2,9 +2,34 @@ package classes;
 
 public class Cliente {
 
-    private int Id;
+    private String idCliente;
     private String name;
     private String email;
     private String address;
+
+    public Cliente(String idCliente, String name, String email, String address) {
+        this.idCliente = idCliente;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    
+    
     
 }
--- src/classes/Pelicula.java
+++ src/classes/Pelicula.java
@@ -4,11 +4,46 @@ import java.time.LocalDate;
 
 public class Pelicula {
     
-    private int Id;
+    private String idPelicula;
     private String title;
     private String director;
-    private LocalDate year;
+    private String year;
     private String genre;
-    private int price;
+    private String price;
+
+    public Pelicula(String idPelicula, String title, String director, String year, String genre, String price) {
+        this.idPelicula = idPelicula;
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.genre = genre;
+        this.price = price;
+    }
+
+    public String getIdPelicula() {
+        return idPelicula;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+    
+    
     
 }
